905dcd30a38021bb17e910b6a808f44820b8f1e5
juninjune <r0otstat1c@naver.com>
2021-09-19 16:32:13 +0900

    From Setup to first view

---
+++ src/controllers/videoController.js
@@ -0,0 +1,12 @@
+export const trending = (req, res) => res.render("home");
+export const upload = (req, res) => res.send("Upload");
+export const see = (req, res) => {
+    return res.send(`Watch Video #${req.params.id}`);
+}
+export const edit = (req, res) => {
+    return res.send("Edit Video");
+}
+export const search = (req, res) => res.send("Search Video");
+export const deleteVideo = (req, res) => {
+return res.send("Delete Video");
+}
