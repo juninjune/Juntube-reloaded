9587f9cf041c837dd6939476423ce8c62962aee6
juninjune <r0otstat1c@naver.com>
2021-09-19 17:11:09 +0900

    so da nae

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,11 +1,7 @@
 export const trending = (req, res) => res.render("home");
 export const upload = (req, res) => res.send("Upload");
-export const see = (req, res) => {
-    return res.send(`Watch Video #${req.params.id}`);
-}
-export const edit = (req, res) => {
-    return res.send("Edit Video");
-}
+export const see = (req, res) => res.render("watch");
+export const edit = (req, res) => res.render("edit");
 export const search = (req, res) => res.send("Search Video");
 export const deleteVideo = (req, res) => {
 return res.send("Delete Video");
